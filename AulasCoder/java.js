b2c720c31dbfbb234c6fc4c1e20bfcc21747a140
Paulo-Estalise <phestalise@gmail.com>
2023-12-13 15:40:39 -0300

    atualizando

---
+++ AulasCoder/java.js
@@ -0,0 +1,11 @@
+
+
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número número:");
+ numero1 = parseInt(numero1);
+ numero2 = parseInt(numero2);
+
+var soma = numero1 + numero2;
+alert("A soma dos dois números é:" + soma);
+//alert ou console.log
+
