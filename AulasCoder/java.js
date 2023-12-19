3737a30ebcfff094e68aa4c1392f9e7035dfcf4a
Paulo-Estalise <143442886+Paulo-Estalise@users.noreply.github.com>
2023-12-19 15:03:34 -0300

    Update java.js

--- AulasCoder/java.js
+++ AulasCoder/java.js
@@ -1,4 +1,8 @@
+// Solicitar ao usuário que digite o nome
+var nome = prompt("Digite seu nome:");
 
+// Exibir o nome no console.log
+console.log("O nome inserido foi: " + nome + "!");
 
 var numero1 = prompt("Digite o primeiro número:");
 var numero2 = prompt("Digite o segundo número número:");
